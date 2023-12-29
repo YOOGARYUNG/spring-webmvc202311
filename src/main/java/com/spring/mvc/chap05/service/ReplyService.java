08d90a338a5527e6c4e481e91eeaedfe83c977e5
GaRyung <shubom0115@naver.com>
2023-12-29 09:36:09 +0900

    # 댓글 등록 회원제 전환

--- src/main/java/com/spring/mvc/chap05/service/ReplyService.java
+++ src/main/java/com/spring/mvc/chap05/service/ReplyService.java
@@ -13,9 +13,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.servlet.http.HttpSession;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static com.spring.mvc.util.LoginUtils.getCurrentLoginMemberAccount;
 
 @Service
 @RequiredArgsConstructor
@@ -45,10 +48,12 @@ public class ReplyService {
     }
 
     // 댓글 등록 서비스
-    public ReplyListResponseDTO register(ReplyPostRequestDTO dto) throws SQLException {
+    public ReplyListResponseDTO register(ReplyPostRequestDTO dto, HttpSession session) throws SQLException {
         log.debug("register services execute!!");
 
         // dto를 entity로 변환
+        Reply reply = dto.toEntity();
+        reply.setAccount(getCurrentLoginMemberAccount(session));
 
         boolean flag = replyMapper.save(dto.toEntity());
         if (!flag) {
